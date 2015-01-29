f319d1a95020162064aac5d271175d7fc4889dff
alexpchin <alex@alexchin.co.uk>
2015-01-29 13:42:52 +0000

    Adding basic node app

---
+++ app.js
@@ -0,0 +1,59 @@
+var morgan  = require('morgan');
+var express = require('express');
+var app     = express();
+var server  = require('http').createServer(app);
+var port    = process.env.PORT || 3000;
+var router  = express.Router();
+var search_term = "ruby"
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+// Setup public folder, similar to sinatra (notice preceeding slash)
+app.use(express.static(__dirname + '/public'));
+
+// Middelware
+app.use(morgan('dev'));
+
+// Routes using Routing engine
+router.get('/', function(req, res) {
+  res.render('index', { header: 'Searching twitter realtime for "'+ search_term +'"' });
+});
+
+// router.get('/contact', function(req, res) {
+//   res.render('contact', { header: 'contact!'});
+// });
+
+// router.get('/about', function(req, res) {
+//   res.render('about', { header: 'about!'});
+// });
+
+// !important 
+app.use('/', router);
+
+server.listen(port);
+console.log('Server started on ' + port);
+
+// Integrate socket
+var io = require('socket.io')(server);
+
+var Twit = require('twit');
+var twitter = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN, 
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+var stream = twitter.stream('statuses/filter', { track: search_term });
+
+io.on('connect', function(socket) {
+  stream.on('tweet', function(tweet) {
+    var data = {};
+    data.name = tweet.user.name;
+    data.screen_name = tweet.user.screen_name;
+    data.text = tweet.text;
+    data.user_profile_image = tweet.user.profile_image_url;
+    socket.emit('tweets', data);
+  });
+});
